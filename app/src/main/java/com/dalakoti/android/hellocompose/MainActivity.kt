b5f8107e2ba58f775f82d5bee13086ca8d92afb6
dalakoti07 <saurabhdalakoti07@gmail.com>
2021-08-12 23:52:55 +0530

    added music knob

--- app/src/main/java/com/dalakoti/android/hellocompose/MainActivity.kt
+++ app/src/main/java/com/dalakoti/android/hellocompose/MainActivity.kt
@@ -1,38 +1,74 @@
 package com.dalakoti.android.hellocompose
 
+
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInParent
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.dalakoti.android.hellocompose.ui.theme.HelloComposeTheme
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.roundToInt
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn {
-                items(5000){
-                    Text(
-                        text = "Item $it",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .border(1.dp, Color.Green, RoundedCornerShape(10.dp))
+                        .padding(30.dp)
+                ) {
+                    var volume by remember {
+                        mutableStateOf(0f)
+                    }
+                    val barCount = 20
+                    MusicKnob(
+                        modifier = Modifier.size(100.dp)
+                    ) {
+                        volume = it
+                    }
+                    Spacer(modifier = Modifier.width(20.dp))
+                    VolumeBar(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(vertical = 24.dp)
+                            .height(30.dp),
+                        activeBars = (barCount * volume).roundToInt(),
+                        barCount = barCount
                     )
                 }
             }
@@ -40,31 +76,90 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-
+@ExperimentalComposeUiApi
 @Composable
-fun SimpleList() {
-    val scrollState = rememberScrollState()
-
-    Column(modifier = Modifier.verticalScroll(scrollState)) {
-        for (i in 1..50) {
-            Text(
-                text = "Item $i",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp)
-            )
+fun VolumeBar(
+    modifier: Modifier = Modifier,
+    activeBars: Int = 0,
+    barCount: Int = 10
+) {
+    BoxWithConstraints(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        val barWidth = remember {
+            constraints.maxWidth / (2f * barCount)
+        }
+        Canvas(modifier = modifier) {
+            for(i in 0 until barCount) {
+                drawRoundRect(
+                    color = if(i in 0..activeBars) Color.Green else Color.DarkGray,
+                    topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
+                    size = Size(barWidth, constraints.maxHeight.toFloat()),
+                    cornerRadius = CornerRadius(0f)
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@ExperimentalComposeUiApi
 @Composable
-fun DefaultPreview() {
-    HelloComposeTheme {
-
+fun MusicKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChange: (Float) -> Unit
+) {
+    var rotation by remember {
+        mutableStateOf(limitingAngle)
     }
+    var touchX by remember {
+        mutableStateOf(0f)
+    }
+    var touchY by remember {
+        mutableStateOf(0f)
+    }
+    var centerX by remember {
+        mutableStateOf(0f)
+    }
+    var centerY by remember {
+        mutableStateOf(0f)
+    }
+
+    Image(
+        painter = painterResource(id = R.drawable.music_knob),
+        contentDescription = "Music knob",
+        modifier = modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow()
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
+            }
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        if (angle !in -limitingAngle..limitingAngle) {
+                            val fixedAngle = if (angle in -180f..-limitingAngle) {
+                                360f + angle
+                            } else {
+                                angle
+                            }
+                            rotation = fixedAngle
+
+                            val percent = (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChange(percent)
+                            true
+                        } else false
+                    }
+                    else -> false
+                }
+            }
+            .rotate(rotation)
+    )
 }
