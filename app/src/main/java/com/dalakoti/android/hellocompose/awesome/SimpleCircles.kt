39cf802acfdf16a24a06ee879810aadf736c9467
dalakoti07 <saurabhdalakoti07@gmail.com>
2023-08-11 23:36:10 +0530

    Added: rotating circles

--- app/src/main/java/com/dalakoti/android/hellocompose/awesome/SimpleCircles.kt
+++ app/src/main/java/com/dalakoti/android/hellocompose/awesome/SimpleCircles.kt
@@ -1,7 +1,12 @@
 package com.dalakoti.android.hellocompose.awesome
 
-import android.util.Log
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +20,9 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,7 +42,8 @@ fun SimpleCircles() {
                 Color.Black
             )
             .verticalScroll(rememberScrollState())
-            .padding(24.dp)
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         allAnimationsList.forEach {example->
             example()
@@ -47,6 +55,41 @@ fun SimpleCircles() {
 private val allAnimationsList=  mutableListOf<@Composable () -> Unit>().apply {
     add {
         HorizontalLoadingPb(3)
+    }
+    add {
+        RotatingCirclesPb(3)
+    }
+
+}
+
+@Composable
+fun RotatingCirclesPb(count: Int) {
+    val infiniteAnimation = rememberInfiniteTransition(label = "rotationRow")
+    val animatingDegree by infiniteAnimation.animateFloat(
+        initialValue = 0f,
+        targetValue = 180f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, delayMillis = 0, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart,
+        ), label = "",
+    )
+    Row(
+        modifier = Modifier
+            .padding(
+                top = 100.dp,
+            )
+            .rotate(
+            animatingDegree
+        )
+    ) {
+        repeat(count){
+            Box(modifier = Modifier
+                .size(50.dp)
+                .background(
+                    color = Color.White,
+                    shape = CircleShape,
+                ))
+        }
     }
 }
 
@@ -73,7 +116,6 @@ fun HorizontalLoadingPb(counter: Int){
 
 @Composable
 fun EachCircle(current: Int, counter: Int) {
-    Log.d(TAG, "EachCircle .... $current and $counter")
     val scale by animateFloatAsState(
         targetValue = (if(current == counter) 1.0 else 0.5).toFloat(),
         label = "pbScale",
