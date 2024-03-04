95bd9b0d0ccaee447665b23cb5f305a619e5beda
Saurabh Dalakoti <saurabhdalakoti@BMLAP317.local>
2024-03-04 16:43:10 +0530

    added jelly fish

--- app/src/main/java/com/dalakoti/android/hellocompose/examples/BasicExamples.kt
+++ app/src/main/java/com/dalakoti/android/hellocompose/examples/BasicExamples.kt
@@ -1,10 +1,13 @@
 package com.dalakoti.android.hellocompose.examples
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
@@ -22,6 +25,13 @@ import com.dalakoti.android.hellocompose.pathExamples.PolygonPathExamples
 import com.dalakoti.android.hellocompose.pathExamples.TicketWaveComposable
 
 private val allExamples = mutableListOf<@Composable () -> Unit>().apply {
+    add {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            JellyfishAnimation()
+        }else{
+            Text(text = "Not supported", color = Color.White,)
+        }
+    }
     add {
         TicketWaveComposable(modifier = Modifier)
     }
